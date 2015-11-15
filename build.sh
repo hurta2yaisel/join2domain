073b1e2befc654eb4e66370521eb8f1e662233f4
Yaisel Hurtado González <yaiselhg@uci.cu>
2015-11-15 12:25:37 -0500

    Relased join2domain v0.1.7...!

--- build.sh
+++ build.sh
@@ -1,3 +1,30 @@
 #!/bin/bash -e
+
+WORK_DIR=$(dirname $0)
+LSM_FILE=$WORK_DIR/join2domain.lsm
+
+OLD_VERSION=$(egrep ^Version: $LSM_FILE)
+NEW_VERSION=$(echo $OLD_VERSION | awk '{
+    split($2,varr,".")
+    print varr[1]"."varr[2]"."varr[3]+1
+}')
+
+sed -i "/^Version:/s/$OLD_VERSION/Version:        $NEW_VERSION/g" $LSM_FILE
+
+if [ "$WORK_DIR" != "." ];then
+    cd $WORK_DIR
+fi
+OLD_RUNS=$(ls *.run)
+rm -f *.run
+
 makeself --xz --notemp --lsm ./join2domain.lsm \
-join2domain/ join2domain.run "join2domain files..." ./join2domain.sh
+join2domain/ join2domain-v$NEW_VERSION.run "join2domain files..." ./join2domain.sh
+
+tar -czvf ../join2domain-v$NEW_VERSION.tar.gz join2domain-v$NEW_VERSION.run
+
+git add *
+git add $OLD_RUNS
+git commit -m "Released join2domain v$NEW_VERSION...!"
+git push origin master
+git tag -a v$NEW_VERSION -m "$NEW_VERSION"
+git push origin v$NEW_VERSION
